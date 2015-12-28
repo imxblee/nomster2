5837e2507e16cbd55a466f8e60b34bff731230d2
imxblee <transcendencez@gmail.com>
2015-12-28 12:58:10 +0000

    set up simpleform and do up new html page layout

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,11 @@
 class PlacesController < ApplicationController
 	def index
 		@place = Place.paginate(:page => params[:page], :per_page => 3)
-
 	end
+	def new
+		@place = Place.new
+		
+	end
+
+
 end
